1ca8924b83fa1686ad73b554347be03642129e7a
Yousra <nice.sora@hotmail.com>
2024-04-29 12:31:00 +1000

    Added apollo/server/express config in main index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,50 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+
+// Import express and express middleware for ApolloServer plugin
+// and cors for enabling cross-origin requests
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
 // Import merged resolvers and typeDefs
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
-// Create a new ApolloServer instance with the merged typeDefs and resolvers
+// Required logic for integrating with Express
+const app = express();
+
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Create a new ApolloServer instance with the merged typeDefs and resolvers,
+// plus the drain plugin for our httpServer.
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+// Ensure we wait for our server to start
+await server.start();
 
-console.log(`🚀 Server ready at ${url}`);
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
