97a2aafa3a0034a09a69051abd93184276934fd4
Yousra <nice.sora@hotmail.com>
2024-05-07 21:46:57 +1000

    Updated index.js for deploying

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,10 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
   }
   app.post("/create-checkout-session", async (req, res) => {
